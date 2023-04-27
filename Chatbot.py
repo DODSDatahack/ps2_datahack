ddf1ccec1817beb3a674a971f5cfd5d147e0541a
SwarJagdale <128358541+SwarJagdale@users.noreply.github.com>
2023-04-28 00:16:18 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,14 @@
+API_KEY='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'
+import openai
+import os
+import streamlit as st
+os.environ['OPENAI_Key']=API_KEY
+openai.api_key=os.environ['OPENAI_Key'] 
+st.title("DODS AI")
+prompt= st.text_input("How can I help you")
+keep_prompting= st.button("Go")
+if keep_prompting:
+    response=openai.Completion.create(engine='text-davinci-003',prompt=prompt, max_tokens=200)
+    st.write(response['choices'][0]['text'])
+    keep_prompting = False
+
